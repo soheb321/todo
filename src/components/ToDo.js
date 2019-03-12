80facb38f5b77b72d1c3781cf3791fdbbf043957
soheb panjwani <sieger@sohebs-MBP.dal.corespace.com>
2019-03-12 12:32:46 -0500

    State-props

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react';
 class ToDo extends Component {
     render() {
      return (
-       <li> A todo will go here </li>
+<li>{ this.props.description }</li>
+ <li>
+         <input type="checkbox" checked={ this.props.isCompleted } />
+         <span>{ this.props.description }</span>
+ </li>
      );
    }
 }
